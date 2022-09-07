3d160eb02ad770fa90595fcb1203b2f02f7769e9
Ar-Mani05 <112660062+Ar-Mani05@users.noreply.github.com>
2022-09-07 01:31:15 -0500

    Added sample java file

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class GitPushJenkin {
+
+public static void main (String [] args)
+{
+System.out.println("Hello Agni");
+System.out.println("Hello Riya");
+System.out.println("Hello Jenkins");
+System.out.println("Hello GitHub");
+}
+}
